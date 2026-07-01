ba8e2ddc54194096d69ec39b4b8b2846d30f28fc
annasilvaa11r@gmail.com <annasilvaa11r@gmail.com>
2026-07-01 17:37:37 -0300

    tarefa 03 (finalização)

--- tarefa03/meuapp/views.py
+++ tarefa03/meuapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def usuarios(request):
+    usuarios = [
+        {"nome": "Maria Matilde", "matricula": "2021001", "idade": 20, "cidade": "Natal"},
+        {"nome": "João Silva", "matricula": "2021002", "idade": 22, "cidade": "Natal"},
+        {"nome": "Ana Souza", "matricula": "2021003", "idade": 19, "cidade": "São Paulo do Potengi"},
+        {"nome": "Pedro Oliveira", "matricula": "2021004", "idade": 21, "cidade": "São Paulo do Potengi"},
+        {"nome": "Carla Santos", "matricula": "2021005", "idade": 23, "cidade": "São Paulo do Potengi"},
+    ]
+    lista_usuarios = {"usuarios": usuarios}
+    return render(request, "usuarios.html", lista_usuarios)
